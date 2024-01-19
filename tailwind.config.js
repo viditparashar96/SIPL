be15410c6136339d3d21f877ee1e6d715f1f68d0
vidit <viditsharma818@gmail.com>
2024-01-19 15:38:17 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+    colors:{
+      primary: '#FEC200',
+      secondary: '#01477F',
+      blue:"#0064B5",
+      border:"#E5E5E5",
+
+    }
+  },
+  plugins: [],
+}
+
